06ab7830cf18c77e410ba1dd9d432d6527ee87f7
andersonpog <silveira.anderson.1988@gmail.com>
2025-10-25 20:31:18 -0300

    funcionalidades basicas completas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_18_062655) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_25_221504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_18_062655) do
     t.text "content"
     t.string "commenter_name"
     t.bigint "movie_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_comments_on_movie_id"
